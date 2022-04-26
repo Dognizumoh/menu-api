24ba55de70125e6137648538f413fbc0f371b3cb
Dognizumoh <idafolo16@gmail.com>
2022-04-26 12:38:06 +0000

    admin role

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const morgan = require("morgan")
 require("dotenv").config()
 const connectDB = require("./config/connectDB")
-const foodRoute = require ("./routes/foodRoutes")
+const foodRoutes = require ("./routes/foodRoutes")
 const userRoute = require("./routes/userRoute");
 
 const app = express();
@@ -11,8 +11,8 @@ connectDB()
 //Middlewares
 app.use(express.json())
 app.use(morgan("dev"))
-app.use(foodRoute)
-app.use(userRoute)
+app.use("/api/foods",foodRoutes);
+app.use("/api/users",userRoute);
 
 const PORT = process.env.PORT||9000
 
--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -3,9 +3,9 @@ const User= require("../models/userSchema")
 
 exports.protect= async function (req, res, next){
     let token;
-    if (req.headers.authorization ){
+    if (req.headers.authorization && req.headers.authorization.startWith("Bearer")){
         try{
-            token = req.headers.authorization;
+            token = req.headers.authorization .spilt(" ")[1];
             const decoded = jwt.verify(token,process.env.JWT_SECRET);
             req.user = await User.findbyId(decoded.id);
             next();
@@ -22,5 +22,18 @@ exports.protect= async function (req, res, next){
             });
         
         }
+       
 
 };
+
+
+ //is admin middleware
+ exports.admin = async function (req,res,next){
+    if(req.user && req.user.isAdmin){
+        next()
+    }else {
+        res.status(401).json({
+            message:"You are not authorized admin",
+        });
+    }
+}
